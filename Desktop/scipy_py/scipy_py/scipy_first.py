644f320f5c7388d17b3d38be8e6c341a3cd8802c
tunnu-sahni <tsahni313@gmail.com>
2025-11-16 18:14:26 +0530

    added scipy_first.py

---
+++ Desktop/scipy_py/scipy_py/scipy_first.py
@@ -0,0 +1,109 @@
+from scipy import constants
+
+print(constants.pi)
+
+
+from scipy import constants
+
+print(constants.pi)
+
+
+from scipy import constants
+
+print(dir(constants))
+
+
+
+from scipy import constants
+
+print(dir(constants))
+
+
+from scipy import constants
+
+print(constants.pi)
+
+
+# metric prefixes
+
+
+from scipy import constants
+
+print(constants.yotta)
+print(constants.zetta)
+print(constants.exa)
+print(constants.peta)
+print(constants.tera)
+print(constants.giga)
+print(constants.mega)
+print(constants.kilo)
+print(constants.hecto)
+print(constants.deka)
+print(constants.deci)
+print(constants.centi)
+print(constants.milli)
+print(constants.micro)
+print(constants.nano)
+print(constants.pico)
+print(constants.femto)
+print(constants.atto)
+print(constants.zepto)
+
+
+
+# BIMARY PREFIXES
+
+
+from scipy import constants
+
+print(constants.kibi)
+print(constants.mebi)
+print(constants.gibi)
+print(constants.tebi)
+print(constants.pebi)
+print(constants.exbi)
+print(constants.zebi)
+print(constants.yobi)
+print(constants.yobi)
+
+
+
+
+# MASS
+
+
+
+from scipy import constants
+
+print(constants.gram)
+print(constants.metric_ton)
+print(constants.grain)
+print(constants.lb)
+print(constants.pound)
+print(constants.oz)
+print(constants.ounce)
+print(constants.stone)
+print(constants.long_ton)
+print(constants.short_ton)
+print(constants.troy_ounce)
+print(constants.troy_ounce)
+print(constants.atomic_mass)
+print(constants.m_u)
+print(constants.u)
+
+
+
+# ANGLE
+
+
+from scipy import constants
+
+print(constants.degree)
+print(constants.arcmin)
+print(constants.arcminute)
+print(constants.arcsec)
+print(constants.arcsecond)
+print(constants.arcsec)
+
+
+
