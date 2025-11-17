44d7f1bd8046e010e2e4a4b268fa7a9efa2d93e0
tunnu-sahni <tsahni313@gmail.com>
2025-11-17 17:40:27 +0530

    added timeconst.py

---
+++ Desktop/scipy_py/scipy_py/timeconst.py
@@ -0,0 +1,148 @@
+# TIME 
+
+
+from scipy import constants
+
+print(constants.minute)
+print(constants.hour)
+print(constants.day)
+print(constants.week)
+print(constants.year)
+print(constants.Julian_year)
+print(constants.Julian_year)
+
+
+
+from scipy import constants
+
+print(constants.minute)
+print(constants.hour)
+print(constants.day)
+print(constants.week)
+print(constants.year)
+print(constants.Julian_year)
+
+
+
+
+# LENGTH
+
+
+from scipy import constants
+
+print(constants.inch)
+print(constants.foot)
+print(constants.yard)
+print(constants.mile)
+print(constants.mil)
+print(constants.pt)
+print(constants.point)
+print(constants.survey_foot)
+print(constants.survey_mile)
+print(constants.nautical_mile)
+print(constants.fermi)
+print(constants.angstrom)
+print(constants.micron)
+print(constants.au)
+print(constants.astronomical_unit)
+print(constants.light_year)
+print(constants.parsec)
+
+
+
+
+#PRESSURE
+
+
+
+from scipy import constants
+
+print(constants.atm)
+print(constants.atmosphere)
+print(constants.bar)
+print(constants.torr)
+print(constants.mmHg)
+print(constants.psi)
+print(constants.mmHg)
+
+
+
+from scipy import constants
+
+print(constants.atm)
+
+
+
+# SPEED
+
+
+
+from scipy import constants
+
+print(constants.kmh)
+print(constants.mph)
+print(constants.mach)
+print(constants.speed_of_sound)
+print(constants.knot)
+
+
+
+#TEMPERATURE
+
+
+from scipy import constants
+
+print(constants.zero_Celsius)
+print(constants.degree_Fahrenheit)
+
+
+
+from scipy import constants
+
+print(constants.zero_Celsius)
+print(constants.degree_Fahrenheit)
+
+
+
+# ENERGY
+
+
+
+from scipy import constants
+
+print(constants.eV)
+print(constants.electron_volt)
+print(constants.calorie)
+print(constants.calorie_th)
+print(constants.calorie_IT)
+print(constants.erg)
+print(constants.Btu)
+print(constants.Btu_th)
+print(constants.ton_TNT)
+
+
+
+
+# POWER
+
+
+
+from scipy import constants
+
+print(constants.hp)
+print(constants.horsepower)
+
+
+
+# FORCE
+
+
+
+from scipy import constants
+
+print(constants.dyn)
+print(constants.dyne)
+print(constants.lbf)
+print(constants.pound_force)
+print(constants.kgf)
+print(constants.kilogram_force)
