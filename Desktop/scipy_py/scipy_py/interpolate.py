24c492aae5778197edbf1c8733963911a20d7daf
tunnu-sahni <tsahni313@gmail.com>
2025-11-23 17:23:20 +0530

    added interpolate.py

---
+++ Desktop/scipy_py/scipy_py/interpolate.py
@@ -0,0 +1,39 @@
+from scipy.interpolate import interp1d
+import numpy as np
+
+xs = np.arange(10)
+ys = 2*xs + 1
+
+interp_func = interp1d(xs, ys)
+
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+
+print(newarr)
+
+
+
+from scipy.interpolate import UnivariateSpline
+import numpy as np
+
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+
+interp_func = UnivariateSpline(xs, ys)
+
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+
+print(newarr)
+
+
+
+from scipy.interpolate import Rbf
+import numpy as np
+
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+
+interp_func = Rbf(xs, ys)
+
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+
+print(newarr) 
