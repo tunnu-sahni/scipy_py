a696c2fcf206b3ee042fc684015177adfcccdc1c
tunnu-sahni <tsahni313@gmail.com>
2025-11-26 20:52:27 +0530

    first commit

---
+++ Desktop/matlib_py/marker.py
@@ -0,0 +1,154 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 8, 1, 10])
+
+# plt.plot(ypoints, marker = 'o')
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 8, 1, 10])
+
+# plt.plot(ypoints, marker = '*')
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 8, 1, 10])
+
+# plt.plot(ypoints, marker = 'P')
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 1, 8, 10])
+
+# plt.plot(ypoints, marker = 'X')
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 1, 8, 10])
+
+# plt.plot(ypoints, marker = 's')
+# plt.show()
+
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 1, 8, 10])
+
+# plt.plot(ypoints, marker = 'D')
+# plt.show()
+
+
+
+# # format string fmt
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 1, 8, 10])
+
+# plt.plot(ypoints, marker = 'o:r')
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 1, 8, 10])
+
+# plt.plot(ypoints, marker = '-')
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 1, 8, 10])
+
+# plt.plot(ypoints, marker = '--')
+# plt.show()
+
+
+
+# marker size
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 1, 8, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20)
+plt.show()
+
+
+# marker color 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 1, 8, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec= 'r')
+plt.show()
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 1, 8, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mfc= 'r')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 1, 8, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
+plt.show()
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 1, 8, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = '#4CAF50', mfc = '#4CAF50')
+plt.show()
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 1, 8, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'hotpink', mfc = 'hotpink')
+plt.show()
