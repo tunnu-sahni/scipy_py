2bb3328a469a30392f354493bf02a3223cccf4e1
tunnu-sahni <tsahni313@gmail.com>
2025-11-27 21:10:33 +0530

    added labeltitle.py

---
+++ Desktop/matlib_py/matlib_first/labeltitle.py
@@ -0,0 +1,72 @@
+# import matplotlib.pyplot as plt
+
+# import numpy as np
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.plot(x, y)
+# plt.xlabel('Average Pulse')
+# plt.ylabel('Calorie Burnage')
+
+# plt.show()
+
+
+
+# # create a title for a plot
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.plot(x, y)
+
+# plt.title('Sports With Data')
+# plt.xlabel('Average Pulse')
+# plt.ylabel('Calorie Burnate')
+
+# plt.show()
+
+
+# # set font properties for title and labels
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# font1 = {'family': 'serif', 'color':'blue', 'size':20}
+# font2 = {'family': 'serif', 'color': 'darkred', 'size':15}
+
+# plt.title('Sports Watch Data', fontdict = font1)
+# plt.xlabel('Average Pulse', fontdict = font2)
+# plt.ylabel('Calorie Burnate', fontdict = font2)
+
+# plt.plot(x , y)
+# plt.show()
+
+
+
+# # position the title
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.title('Sports Watch Data', loc = 'left')
+# plt.ylabel('Calorie Burnate')
+
+# plt.plot(x , y)
+# plt.show()
+
+
+
+
